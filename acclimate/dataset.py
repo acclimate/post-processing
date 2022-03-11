6d00997a35f53d44191ea378bbff3a6e90923ae0
Robin Middelanis <robin.middelanis@pik-potsdam.de>
2022-03-11 09:34:13 +0100

    added operator overloading

--- acclimate/dataset.py
+++ acclimate/dataset.py
@@ -17,39 +17,58 @@ class AcclimateOutput:
             self._agent_subcoords = agent_subcoords
         elif filename is not None:
             self._data = xr.Dataset()
-            for i_group in ['firms', 'regions']:
-                try:
-                    with xr.open_dataset(filename, group=i_group) as _data:
-                        _data = _data.rename({v: "{}_{}".format(i_group, v) for v in list(_data.variables)})
-                        self._data.update(_data)
-                except OSError as e:
-                    print("OS error: {0}".format(e))
-            with Dataset(filename, 'r') as ncdata:
-                agent_names = [str(a[0].decode('UTF-8')) for a in ncdata['agent'][:]]
-                agent_types = [ncdata['agent_type'][a[1]] for a in ncdata['agent'][:]]
-                agent_sectors = [ncdata['sector'][a[2]] for a in ncdata['agent'][:]]
-                agent_regions = [ncdata['region'][a[3]] for a in ncdata['agent'][:]]
-                for idx in range(len(agent_names)):
-                    agent_names[idx] = agent_names[idx].split(':')[0] + ":{}".format(agent_regions[idx])
-                self._agent_coords = agent_names
-                self._agent_subcoords = {
-                    'region': {a: r for a, r in zip(agent_names, agent_regions)},
-                    'sector': {a: s if t == 'firm' else 'FCON' for a, s, t in
-                               zip(agent_names, agent_sectors, agent_types)},
-                    'agent_type': {a: t for a, t in zip(agent_names, agent_types)}
-                }
-                if start_date is None:
-                    start_date = ncdata['time'].units.split(' ')[-1]
-                coords = {
-                    'time': pd.date_range(start_date, periods=len(ncdata['time']), freq='D'),
-                    'agent': agent_names,
-                    'region': ncdata['region'][:],
-                }
-            for coord, ticks in coords.items():
-                self._data[coord] = ticks
-            self._baseline = self._data.sel(time=self._data.time[0])
+            self.load_dataset(filename=filename, start_date=start_date)
         else:
             raise ValueError("Either both or none of data and filename were passed.")
+
+    @property
+    def data(self):
+        return self._data
+
+    @property
+    def baseline(self):
+        return self._baseline
+
+    @property
+    def agent_coords(self):
+        return self._agent_coords
+
+    @property
+    def agent_subcoords(self):
+        return self._agent_subcoords
+
+    def load_dataset(self, filename, start_date):
+        for i_group in ['firms', 'regions']:
+            try:
+                with xr.open_dataset(filename, group=i_group) as _data:
+                    _data = _data.rename({v: "{}_{}".format(i_group, v) for v in list(_data.variables)})
+                    self._data.update(_data)
+            except OSError as e:
+                print("OS error: {0}".format(e))
+        with Dataset(filename, 'r') as ncdata:
+            agent_names = [str(a[0].decode('UTF-8')) for a in ncdata['agent'][:]]
+            agent_types = [ncdata['agent_type'][a[1]] for a in ncdata['agent'][:]]
+            agent_sectors = [ncdata['sector'][a[2]] for a in ncdata['agent'][:]]
+            agent_regions = [ncdata['region'][a[3]] for a in ncdata['agent'][:]]
+            for idx in range(len(agent_names)):
+                agent_names[idx] = agent_names[idx].split(':')[0] + ":{}".format(agent_regions[idx])
+            self._agent_coords = agent_names
+            self._agent_subcoords = {
+                'region': {a: r for a, r in zip(agent_names, agent_regions)},
+                'sector': {a: s if t == 'firm' else 'FCON' for a, s, t in
+                           zip(agent_names, agent_sectors, agent_types)},
+                'agent_type': {a: t for a, t in zip(agent_names, agent_types)}
+            }
+            if start_date is None:
+                start_date = ncdata['time'].units.split(' ')[-1]
+            coords = {
+                'time': pd.date_range(start_date, periods=len(ncdata['time']), freq='D'),
+                'agent': agent_names,
+                'region': ncdata['region'][:],
+            }
+        for coord, ticks in coords.items():
+            self._data[coord] = ticks
+        self._baseline = self._data.sel(time=self._data.time[0])
 
     def get_agents(self, sector=None, region=None, agent_type=None):
         def remap(l, lookupdict):
@@ -133,3 +152,23 @@ class AcclimateOutput:
     def __getitem__(self, item):
         return AcclimateOutput(data=self._data[item], baseline=self._baseline[item], agent_coords=self._agent_coords,
                                agent_subcoords=self._agent_subcoords)
+
+    def __add__(self, other):
+        return AcclimateOutput(data=self.data + other.data, baseline=self.baseline + other.baseline,
+                               agent_coords=self._agent_coords, agent_subcoords=self._agent_subcoords)
+
+    def __sub__(self, other):
+        return AcclimateOutput(data=self.data - other.data, baseline=self.baseline - other.baseline,
+                               agent_coords=self._agent_coords, agent_subcoords=self._agent_subcoords)
+
+    def __mul__(self, other):
+        return AcclimateOutput(data=self.data * other.data, baseline=self.baseline * other.baseline,
+                               agent_coords=self._agent_coords, agent_subcoords=self._agent_subcoords)
+
+    def __pow__(self, other):
+        return AcclimateOutput(data=self.data ** other.data, baseline=self.baseline ** other.baseline,
+                               agent_coords=self._agent_coords, agent_subcoords=self._agent_subcoords)
+
+    def __truediv__(self, other):
+        return AcclimateOutput(data=self.data / other.data, baseline=self.baseline / other.baseline,
+                               agent_coords=self._agent_coords, agent_subcoords=self._agent_subcoords)
