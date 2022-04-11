8965fa7608d47c7f57157bd715271cf9f56dc41d
Robin Middelanis <robin.middelanis@pik-potsdam.de>
2022-04-11 18:46:01 +0200

    added group_agents(...)

--- acclimate/dataset.py
+++ acclimate/dataset.py
@@ -6,17 +6,13 @@ import numpy as np
 
 
 class AcclimateOutput:
-    def __init__(self, filename=None, start_date=None, data=None, baseline=None, agent_coords=None,
-                 agent_subcoords=None, old_output_format=False):
+    def __init__(self, filename=None, start_date=None, data=None, baseline=None, old_output_format=False):
         if data is not None:
-            if agent_coords is None or agent_subcoords is None:
-                raise ValueError("Must pass agent coordinates and subcoordinates if data is passed.")
             self._data = data
             self._baseline = baseline
-            self._agent_coords = agent_coords
-            self._agent_subcoords = agent_subcoords
         elif filename is not None:
             self._data = xr.Dataset()
+            self._baseline = xr.Dataset()
             self.load_dataset(filename=filename, start_date=start_date)
         else:
             raise ValueError("Either both or none of data and filename were passed.")
@@ -32,10 +28,6 @@ class AcclimateOutput:
     @property
     def agent_coords(self):
         return self._agent_coords
-
-    @property
-    def agent_subcoords(self):
-        return self._agent_subcoords
 
     def load_dataset(self, filename, start_date):
         for i_group in ['firms', 'regions']:
@@ -53,35 +45,31 @@ class AcclimateOutput:
             for idx in range(len(agent_names)):
                 agent_names[idx] = agent_names[idx].split(':')[0] + ":{}".format(agent_regions[idx])
             self._agent_coords = agent_names
-            self._agent_subcoords = {
-                'region': {a: r for a, r in zip(agent_names, agent_regions)},
-                'sector': {a: s if t == 'firm' else 'FCON' for a, s, t in
-                           zip(agent_names, agent_sectors, agent_types)},
-                'agent_type': {a: t for a, t in zip(agent_names, agent_types)}
-            }
             if start_date is None:
                 start_date = ncdata['time'].units.split(' ')[-1]
             coords = {
                 'time': pd.date_range(start_date, periods=len(ncdata['time']), freq='D'),
                 'agent': agent_names,
+                'agent_region': ('agent', agent_regions),
+                'agent_sector': ('agent', [s if t == 'firm' else 'FCON' for s, t in zip(agent_sectors, agent_types)]),
+                'agent_type': ('agent', agent_types),
                 'region': ncdata['region'][:],
             }
-        for coord, ticks in coords.items():
-            self._data[coord] = ticks
+        self._data = self._data.assign_coords(coords)
         self._baseline = self._data.sel(time=self._data.time[0])
 
     def get_agents(self, sector=None, region=None, agent_type=None):
-        def remap(l, lookupdict):
-            if l is not None:
-                if type(l) is str:
-                    return [a for a, v in self._agent_subcoords[lookupdict].items() if v == l]
-                elif type(l) is list:
-                    return [a for a, v in self._agent_subcoords[lookupdict].items() if v in l]
-            else:
-                return list(self._agent_subcoords[lookupdict].keys())
-
-        return list(set(remap(sector, 'sector')) & set(remap(region, 'region')) & set(remap(agent_type, 'agent_type')) &
-                    set(self._data.agent.values))
+        if 'agent' in self:
+            def selector(v, lookup):
+                if v is not None:
+                    return np.isin(lookup, v)
+                else:
+                    return np.array([True] * len(lookup))
+            return self['agent'].values[selector(sector, self['agent_sector'].values) &
+                                        selector(region, self['agent_region'].values) &
+                                        selector(agent_type, self['agent_type'].values)]
+        else:
+            print("No dimension 'agent'")
 
     def sel(self, inplace=False, **kwargs):
         if 'agent' in self.coords:
@@ -108,8 +96,42 @@ class AcclimateOutput:
             self._baseline = self._baseline.sel(**kwargs)
         else:
             return AcclimateOutput(data=self._data.sel(**kwargs),
-                                   baseline=self._baseline.sel(**{k: v for k, v in kwargs.items() if k != 'time'}),
-                                   agent_coords=self._agent_coords, agent_subcoords=self._agent_subcoords)
+                                   baseline=self._baseline.sel(**{k: v for k, v in kwargs.items() if k != 'time'}))
+
+    def group_agents(self, dim: str, mapping: dict = None, how: str = 'sum', drop: bool = False, inplace: bool = False):
+        if dim not in ['region', 'sector']:
+            raise ValueError("Cannot group along dimension {}".format(dim))
+        coords = {c: ('agent', c_.values) for c, c_ in self.coords.items() if c in
+                  ['agent', 'agent_sector', 'agent_region', 'agent_type']}
+        for k, v in mapping.items():
+            if k in self.__getattr__('agent_' + dim):
+                print("{} already exists in dimension {}".format(k, 'agent_' + dim))
+                continue
+            vals_to_group = [_v for _v in v if _v in self['agent_' + dim]]
+            if len(vals_to_group) == 0:
+                print("One or more of the values for new entry '{}' could not be found in dim '{}'".format(k, dim))
+            elif len(vals_to_group) < len(v):
+                print("None of the values for new entry '{}' could not be found in dim '{}'".format(k, dim))
+            agents_to_group = self.get_agents(**{dim: vals_to_group})
+            mixed_agent_types = (dim == 'sector' and len(v) > 1 and 'FCON' in v)
+            for a in agents_to_group:
+                agent_index = np.where(self['agent'].values == a)[0][0]
+                if dim == 'sector':
+                    coords['agent'][1][agent_index] = "{}:{}".format(a.split(':')[1], k)
+                    coords['agent_sector'][1][agent_index] = k
+                elif dim == 'region':
+                    coords['agent'][1][agent_index] = "{}:{}".format(k, a.split(':')[0])
+                    coords['agent_region'][1][agent_index] = k
+                if mixed_agent_types:
+                    coords['agent_type'][1][agent_index] = 'mixed'
+        new_coords = xr.Dataset(coords).groupby('agent').first().set_coords(['agent_sector', 'agent_region', 'agent_type']).coords
+        if inplace:
+            self._data = getattr(self._data.assign_coords(coords).groupby('agent'), how)().assign_coords(new_coords)
+            self._baseline = getattr(self._baseline.assign_coords(coords).groupby('agent'), how)().assign_coords(new_coords)
+        else:
+            res_data = getattr(self._data.assign_coords(coords).groupby('agent'), how)().assign_coords(new_coords)
+            res_baseline = getattr(self._baseline.assign_coords(coords).groupby('agent'), how)().assign_coords(new_coords)
+            return AcclimateOutput(data=res_data, baseline=res_baseline)
 
     def _wrapper_func(self, func, inplace=False, *args, **kwargs):
         res = getattr(self._data, func)(*args, **kwargs)
@@ -117,33 +139,21 @@ class AcclimateOutput:
             if inplace:
                 self._data = res
             else:
-                return AcclimateOutput(data=res, baseline=self._baseline, agent_coords=self._agent_coords,
-                                       agent_subcoords=self._agent_subcoords)
+                return AcclimateOutput(data=res, baseline=self._baseline)
         else:
             return res
 
     def __repr__(self):
         _repr = super().__repr__() + "\n" + self._data.__repr__()
-        if 'agent' in self._data.coords:
-            agent_values = self._data.agent.values
-            if agent_values.shape == ():
-                agent_values = [agent_values]
-            subindex_repr = ""
-            for k, v in self._agent_subcoords.items():
-                subindex_repr += "\n   - {}\t({})".format(k, len(set([v[a] for a in agent_values])))
-            index_pos = _repr.find('\n  * agent') + 2
-            if _repr[index_pos:].find('\n') > 0:
-                index_pos += _repr[index_pos:].find('\n')
-            else:
-                index_pos = len(_repr)
-            return _repr[:index_pos] + subindex_repr + _repr[index_pos:]
+        _repr = _repr.replace('agent_region', '- region    ')
+        _repr = _repr.replace('agent_sector', '- sector    ')
+        _repr = _repr.replace('agent_type', '- type    ')
         return _repr
 
     def __getattr__(self, attr):
         if hasattr(self._data, attr):
             if attr in self._data:
-                return AcclimateOutput(data=getattr(self._data, attr), baseline=getattr(self._baseline, attr),
-                                       agent_coords=self._agent_coords, agent_subcoords=self._agent_subcoords)
+                return AcclimateOutput(data=getattr(self._data, attr), baseline=getattr(self._baseline, attr))
             elif hasattr(getattr(self._data, attr), '__call__'):
                 def res(*args, **kwargs):
                     return self._wrapper_func(attr, False, *args, **kwargs)
@@ -153,48 +163,43 @@ class AcclimateOutput:
             return res
 
     def __getitem__(self, item):
-        return AcclimateOutput(data=self._data[item], baseline=self._baseline[item], agent_coords=self._agent_coords,
-                               agent_subcoords=self._agent_subcoords)
+        return AcclimateOutput(data=self._data[item], baseline=self._baseline[item])
 
     def __add__(self, other):
-        return AcclimateOutput(data=self.data + (other.data if type(other) is self.__class__ else other), baseline=self.baseline + other.baseline,
-                               agent_coords=self._agent_coords, agent_subcoords=self._agent_subcoords)
+        return AcclimateOutput(data=self.data + (other.data if type(other) is self.__class__ else other),
+                               baseline=self.baseline + other.baseline)
 
     def __sub__(self, other):
-        return AcclimateOutput(data=self.data - (other.data if type(other) is self.__class__ else other), baseline=self.baseline - other.baseline,
-                               agent_coords=self._agent_coords, agent_subcoords=self._agent_subcoords)
+        return AcclimateOutput(data=self.data - (other.data if type(other) is self.__class__ else other),
+                               baseline=self.baseline - other.baseline)
 
     def __mul__(self, other):
-        return AcclimateOutput(data=self.data * (other.data if type(other) is self.__class__ else other), baseline=self.baseline * other.baseline,
-                               agent_coords=self._agent_coords, agent_subcoords=self._agent_subcoords)
+        return AcclimateOutput(data=self.data * (other.data if type(other) is self.__class__ else other),
+                               baseline=self.baseline * other.baseline)
 
     def __pow__(self, other):
-        return AcclimateOutput(data=self.data ** (other.data if type(other) is self.__class__ else other), baseline=self.baseline ** other.baseline,
-                               agent_coords=self._agent_coords, agent_subcoords=self._agent_subcoords)
+        return AcclimateOutput(data=self.data ** (other.data if type(other) is self.__class__ else other),
+                               baseline=self.baseline ** other.baseline)
 
     def __truediv__(self, other):
-        return AcclimateOutput(data=self.data / (other.data if type(other) is self.__class__ else other), baseline=self.baseline / other.baseline,
-                               agent_coords=self._agent_coords, agent_subcoords=self._agent_subcoords)
+        return AcclimateOutput(data=self.data / (other.data if type(other) is self.__class__ else other),
+                               baseline=self.baseline / other.baseline)
 
     def __le__(self, other):
         return AcclimateOutput(data=self.data <= (other.data if type(other) is self.__class__ else other),
-                               baseline=self.baseline, agent_coords=self._agent_coords,
-                               agent_subcoords=self._agent_subcoords)
+                               baseline=self.baseline)
 
     def __lt__(self, other):
         return AcclimateOutput(data=self.data < (other.data if type(other) is self.__class__ else other),
-                               baseline=self.baseline, agent_coords=self._agent_coords,
-                               agent_subcoords=self._agent_subcoords)
+                               baseline=self.baseline)
 
     def __ge__(self, other):
         return AcclimateOutput(data=self.data >= (other.data if type(other) is self.__class__ else other),
-                               baseline=self.baseline, agent_coords=self._agent_coords,
-                               agent_subcoords=self._agent_subcoords)
+                               baseline=self.baseline)
 
     def __gt__(self, other):
         return AcclimateOutput(data=self.data > (other.data if type(other) is self.__class__ else other),
-                               baseline=self.baseline, agent_coords=self._agent_coords,
-                               agent_subcoords=self._agent_subcoords)
+                               baseline=self.baseline)
 
     def __contains__(self, item):
         data_contains = item in self._data
