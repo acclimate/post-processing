d7e7150bd4adcd21674a1b4b236cf1897034e2c7
Robin Middelanis <robin.middelanis@pik-potsdam.de>
2022-09-19 17:40:46 +0200

    new mapping func with centroid data option

--- acclimate/mapping.py
+++ acclimate/mapping.py
@@ -11,7 +11,7 @@ import numpy as np
 import pyproj
 from matplotlib.collections import PatchCollection
 from matplotlib.colors import LinearSegmentedColormap, Normalize
-from matplotlib.patches import PathPatch
+from matplotlib.patches import PathPatch, Wedge
 from matplotlib.path import Path
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from shapely.geometry import Point
@@ -48,9 +48,193 @@ def create_colormap(name, colors, alphas=None, xs=None):
     return res
 
 
+def make_map(
+        patchespickle_file,
+        map_regions=None,
+        map_data=None,
+        centroids_regions=None,
+        centroids_data=None,
+        centroids_data_unit=None,
+        centroids_annotate=None,
+        cm=None,
+        symmetric_cmap=False,
+        outfile=None,
+        extend_c='both',
+        numbering=None,
+        y_label=None,
+        rasterize=True,
+        min_lon=-156,
+        max_lon=170,
+        min_lat=-58,
+        max_lat=89,
+        valid_ec='black',
+        inv_fc='lightgrey',
+        inv_ec='black',
+        map_v_limits=None,
+        centroids_v_limits=None,
+):
+    if map_data is None and centroids_data is None:
+        raise ValueError("Must pass at least one of map_data and centroids_data to plot.")
+
+    if centroids_annotate is None:
+        centroids_annotate = []
+
+    patchespickle = pickle.load(gzip.GzipFile(patchespickle_file, "rb"))
+    patches = patchespickle["patches"]
+    centroids = patchespickle["centroids"]
+    projection_name = patchespickle["projection"]
+
+    if cm is None:
+        cm = create_colormap("custom", ["red", "white", "blue"], xs=[0, 0.5, 1])
+
+    def my_transform(scale, t, trans, x, y):
+        p = trans(x, y)
+        return (p[0] * scale + t[0], p[1] * scale + t[1])
+
+    def get_projection(to, scale=1, translate=(0, 0)):
+        return partial(
+            my_transform,
+            scale,
+            translate,
+            partial(
+                pyproj.transform,
+                pyproj.Proj("+proj=lonlat +datum=WGS84 +no_defs"),
+                pyproj.Proj(f"+proj={to} +datum=WGS84 +no_defs"),
+            ),
+        )
+
+    projection = get_projection(projection_name)
+
+    minx = transform(projection, Point(min_lon, 0)).x
+    maxx = transform(projection, Point(max_lon, 0)).x
+    miny = transform(projection, Point(0, min_lat)).y
+    maxy = transform(projection, Point(0, max_lat)).y
+
+    fig, (ax, cax) = plt.subplots(
+        ncols=2,
+        gridspec_kw={'width_ratios': [.975, .025]},
+        figsize=(14, 13 / 2)
+    )
+
+    ax.set_aspect(1)
+    ax.axis("off")
+
+    ax.set_xlim((minx, maxx))
+    ax.set_ylim((miny, maxy))
+
+    validpatches = {}
+    validpatches_data = {}
+    invpatches = {}
+    if map_data is not None:
+        if map_v_limits is None:
+            map_vmin = np.min(map_data)
+            map_vmax = np.max(map_data)
+        else:
+            (map_vmin, map_vmax) = map_v_limits
+        if symmetric_cmap and np.sign(map_vmin) != np.sign(map_vmax):
+            v = max(abs(map_vmin), abs(map_vmax))
+            (map_vmin, map_vmax) = (np.sign(map_vmin) * v, np.sign(map_vmax) * v)
+        norm_color = Normalize(vmin=map_vmin, vmax=map_vmax)
+        for r, d in zip(map_regions, map_data):
+            if r in patches:
+                level, subregions, patch = patches[r]
+                if math.isnan(d):
+                    invpatches[r] = patch
+                    print('NAN data for region {}'.format(r))
+                else:
+                    validpatches[r] = patch
+                    validpatches_data[r] = d
+            else:
+                print(f"Region {r} not found in patches.")
+        valid_collection = ax.add_collection(
+            PatchCollection(
+                list(validpatches.values()),
+                edgecolors=valid_ec,
+                facecolors="black",
+                linewidths=.1,
+                rasterized=rasterize,
+                zorder=1
+            )
+        )
+        valid_collection.set_facecolors(cm(norm_color([validpatches_data[k] for k in validpatches.keys()])))
+        cbar = matplotlib.colorbar.ColorbarBase(
+            cax,
+            cmap=cm,
+            norm=norm_color,
+            orientation="vertical",
+            spacing="proportional",
+            extend=extend_c,
+        )
+        cbar.minorticks_on()
+
+    for r, (level, subregions, patch) in patches.items():
+        if len(subregions) == 1 and r not in validpatches and r not in invpatches:
+            invpatches[r] = patch
+            print('No data passed for region {}'.format(r))
+
+    ax.add_collection(
+        PatchCollection(
+            list(invpatches.values()),
+            hatch="///",
+            facecolors=inv_fc,
+            edgecolors=inv_ec,
+            linewidths=.1,
+            rasterized=rasterize,
+            zorder=0
+        )
+    )
+
+    if centroids_data is not None:
+        if centroids_v_limits is None:
+            centroids_vmin = np.min(centroids_data)
+            centroids_vmax = np.max(centroids_data)
+        else:
+            (centroids_vmin, centroids_vmax) = centroids_v_limits
+
+        def get_radius(_d):
+            return .05 * _d / centroids_vmax * (abs(ax.get_ylim()[0]) + abs(ax.get_ylim()[1]))
+        wedges = []
+        for r, d in zip(centroids_regions, centroids_data):
+            if r in centroids:
+                wedges.append(Wedge(centroids[r], get_radius(d), 0, 360))
+                if r in centroids_annotate:
+                    ax.text(centroids[r].x, centroids[r].y, r, ha='center', va='center')
+        # legend_wedges = []
+        x_pos = ax.get_xlim()[0] + get_radius(centroids_vmin)
+        y_pos = ax.get_ylim()[0] + get_radius(centroids_vmax)
+        for d in np.linspace(centroids_vmin, centroids_vmax, 3):
+            radius = get_radius(d)
+            x_pos += 2 * radius
+            wedges.append(Wedge((x_pos, y_pos), radius, 0, 360))
+            text = f"{int(np.round(d, 0))}"
+            if centroids_data_unit is not None:
+                text += "{}".format(centroids_data_unit)
+            ax.text(x_pos, y_pos, text, ha='center', va='center')
+        ax.add_collection(
+            PatchCollection(
+                wedges,
+                facecolors='lightgrey',
+                edgecolors='black',
+                linewidths=.1,
+                rasterized=rasterize,
+                zorder=1
+            )
+        )
+
+    plt.tight_layout()
+
+    if numbering is not None:
+        ax.text(
+            0.0, 1.0, numbering, transform=ax.transAxes, fontweight='bold'
+        )
+
+    if outfile is not None:
+        fig.savefig(outfile, dpi=300)
+
+
 # pathespickle files as well as the script to generate these can be found on the cluster at
 # /p/projects/acclimate/data/plotting
-def make_map(
+def make_map_deprecated(
         patchespickle_file,
         regions,
         data,
@@ -77,7 +261,6 @@ def make_map(
         y_tick_labels=None,
         y_ticks_fontsize=8,
         lims=None,
-        only_usa=False,
         v_limits=None
 ):
     if ignore_regions is None:
@@ -152,24 +335,10 @@ def make_map(
 
     invpatches = []
     validpatches = []
-    regions_with_data = set([])
 
     for r, d in zip(regions, data):
         if r in patches:
             level, subregions, patch = patches[r]
-            if only_usa:
-                if r in 'US.AK':
-                    patch.set_transform(patch.get_transform() + matplotlib.transforms.Affine2D().scale(
-                        0.4) + matplotlib.transforms.ScaledTranslation(transform(projection, Point(-25, 0)).x,
-                                                                       transform(projection, Point(0, -14)).y,
-                                                                       patch.get_transform()))  #
-
-                elif r == 'US.HI':
-                    patch.set_transform(
-                        patch.get_transform() + matplotlib.transforms.ScaledTranslation(
-                            transform(projection, Point(-15, 0)).x,
-                            transform(projection, Point(0, -18)).y,
-                            patch.get_transform()))
             if math.isnan(d):
                 validpatches.append(EmptyPatch())
                 invpatches.append(patch)
@@ -180,7 +349,6 @@ def make_map(
                 print('Ignore region {}'.format(r))
             else:
                 validpatches.append(patch)
-            regions_with_data.update(subregions)
         else:
             validpatches.append(EmptyPatch())
 
